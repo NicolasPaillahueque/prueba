04a96b35acacaed1c031eb96957ad2f6fd9f9843
NicolasPaillahueque <116034185+NicolasPaillahueque@users.noreply.github.com>
2022-11-11 10:50:57 -0300

    Update Interfaz de usuario.py

--- Interfaz de usuario.py
+++ Interfaz de usuario.py
@@ -1,2 +1,28 @@
+from tkinter import ttk 
 from tkinter import *
+
+class usuario:
+    def __init__(self, root):
+        self.wind = root 
+        self.wind.title("Usuario")
+        self.wind.geometry("850x600")
+        self.wind.config(bg="teal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    root = Tk()
+    usuario = usuario(root)
+    root.mainloop()
+
 
