946a2f1f873aa4a4cc92926b8e4df822323f085b
NicolasPaillahueque <nicolas.paillahueque@gmail.com>
2022-12-20 17:27:13 -0300

    Cambios

--- a=3.py
+++ a=3.py
@@ -1,7 +1,23 @@
-a=3
-c=2
+import unittest
+from tkinter import Tk, Button
 
-t=a+c
+class TestButton(unittest.TestCase):
+    def setUp(self):
+        self.root = Tk()
+        self.button = Button(self.root, text="Click me")
+        self.button.pack()
 
-print (t)
+    def test_button_click(self):
+        def callback():
+            self.button["text"] = "Clicked"
 
+        self.button["command"] = callback
+        self.button.invoke()
+
+        self.assertEqual(self.button["text"], "Clicked")
+
+    def tearDown(self):
+        self.root.destroy()
+
+if __name__ == '__main__':
+    unittest.main()
