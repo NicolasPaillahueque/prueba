51f6cacb0afcf20ae58a3e38b167c15d524682f1
NicolasPaillahueque <nicolas.paillahueque@gmail.com>
2022-12-20 17:29:12 -0300

    Interfaz de Usuario

--- Interfaz de usuario.py
+++ Interfaz de usuario.py
@@ -1,31 +1,34 @@
 from tkinter import ttk 
 from tkinter import *
 
-class usuario:
-    def __init__(self, root):
-        self.wind = root 
-        self.wind.title("Login")
-        self.wind.geometry("850x600")
-        self.wind.config(bg="teal")
+root=Tk()
+root.title("Login")
+root.geometry("300x150")
+root.resizable(width=False, height=False)
+root.config(bg="Blue")
 
-        frame1 = LabelFrame(self.wind, text="Nombre de Usuario", font=("calibri", 14))
-        frame2 = LabelFrame(self.wind, text="Contraseña de Usuario", font=("calibri", 14))
+usuario=Label(root, text="Ingrese nombre de usuario:")
+usuario.pack()
 
-        frame1.pack(fill="both", expand="yes", padx=20, pady=15)
-        frame2.pack(fill="both", expand="yes", padx=20, pady=15)
-        lbl1 = Label(frame1, text="ID del Usuario", width=20)
-        lbl1.grid(row=0, column=0, padx=5, pady=3)
-        self.ent1 = Entry(frame1)
-        self.ent1.grid(row=0, column=1, padx=5, pady=3)
+usuario1=StringVar()
+usu=Entry(root, width=30, textvariable=usuario1)
+usu.pack()
 
-       
-        lbl2 = Label(frame1, text="Contraseña del Usuario", width=20)
-        lbl2.grid(row=2, column=0, padx=5, pady=3)
-        self.ent2 = Entry(frame1)
-        self.ent2.grid(row=2, column=1, padx=5, pady=3)
+contraseña=Label(root, text="Contraseña:")
+contraseña.pack()
+
+contra=StringVar()
+contra1=Entry(root, width=30, textvariable=contra, show="*")
+contra1.pack()
+
+def ingresar():
+    if usuario1.get()=="programador" and contra.get()=="123456":
+        root.title("Correcto")
+    else:
+        root.title("Incorrecto")
+
+bt=Button(root, text="Entrar", command=ingresar)
+bt.pack(side=BOTTOM)
 
 
-if __name__=='__main__':
-    root = Tk()
-    usuario = usuario(root)
-    root.mainloop()
+root.mainloop()
