8b9f131bb9ef251a4c1654499ea6a916158e10ef
NicolasPaillahueque <nicolas.paillahueque@gmail.com>
2023-01-03 14:12:48 -0300

    t

---
+++ calc.py
@@ -0,0 +1,25 @@
+
+
+
+def add(x, y):
+    return x + y
+
+def add2(x+y):
+    return x+y
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / float(y)
